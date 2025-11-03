8f822464f55d53c1f952b64c20393336f2f639ea
BorisP <117676537+Lowinne@users.noreply.github.com>
2025-11-03 16:53:17 +0100

    Add Contact model with validation method

---
+++ internal/contacts/model.go
@@ -0,0 +1,11 @@
+package contacts
+
+type Contact struct {
+    ID    int    json:"id"
+    Nom   string json:"nom"
+    Email string json:"email"
+}
+
+func (c Contact) IsValid() bool {
+    return c.Nom != "" && c.Email != ""
+}
